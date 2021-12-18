728cf819b0e1ae30ed5529fe072ad4c112907887
sachin angral <64680762+SachinAngral@users.noreply.github.com>
2021-12-18 12:58:37 +0530

    Update AddressBook.java

--- src/test/java/com/qa/opencart/tests/AddressBook.java
+++ src/test/java/com/qa/opencart/tests/AddressBook.java
@@ -1,1 +1,12 @@
+package com.qa.opencart.tests;
 
+public class AddressBook {
+
+	
+	String name;
+	String address;
+  
+	public void addAddress() {
+		System.out.println("address is displayed")
+	}
+}
